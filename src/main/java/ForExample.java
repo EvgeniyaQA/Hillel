f8e2ef7c85cc9374da5183572b5c02e3e78d9fc2
EvgeniyaQA <32173562+EvgeniyaQA@users.noreply.github.com>
2020-03-13 20:36:05 +0200

    Update ForExample.java

--- src/main/java/ForExample.java
+++ src/main/java/ForExample.java
@@ -1,14 +1,23 @@
+mport javax.crypto.spec.PSource;
+
 public class ForExample {
     public static void main(String[] args) {
         /*for(int i=10; i>0;i--){
             System.out.println(i);
         }*/
-        for (int i=0; i<10;i++){
+       /* for (int i=0; i<10;i++){
             if( i==5)
             break;
             System.out.println(i);
         }
+        System.out.println("За циклом");*/
+
+        for (int a=0;a<10;a++){
+            if (a>=5&&a<=7)// перескок 5 и 7
+                continue;
+            System.out.println(a);
+        }
         System.out.println("За циклом");
     }
-
+    
 }
