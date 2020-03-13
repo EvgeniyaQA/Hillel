7a677cabb8f19ec6377c440882ff1a3705b54aab
EvgeniyaQA <32173562+EvgeniyaQA@users.noreply.github.com>
2020-03-13 20:31:42 +0200

    Add files via upload

---
+++ src/main/java/ForExample.java
@@ -0,0 +1,14 @@
+public class ForExample {
+    public static void main(String[] args) {
+        /*for(int i=10; i>0;i--){
+            System.out.println(i);
+        }*/
+        for (int i=0; i<10;i++){
+            if( i==5)
+            break;
+            System.out.println(i);
+        }
+        System.out.println("За циклом");
+    }
+
+}
